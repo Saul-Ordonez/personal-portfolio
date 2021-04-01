4a01e06400849fb706ec65d97c44435876229019
Saul-Ordonez <sauleordonez@gmail.com>
2021-04-01 11:43:35 -0700

    add pages and route them for navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,6 +14,30 @@ export default function Home() {
         <div className={styles.header}>
           <h1>Saul Ordonez</h1>
         </div>
+      </div>
+
+      <div>
+        <h2>
+          <Link href="/about-me">
+            About Me
+          </Link>
+        </h2>
+      </div>
+
+      <div>
+        <h2>
+          <Link href="/projects">
+            Projects
+          </Link>
+        </h2>
+      </div>
+
+      <div>
+        <h2>
+          <Link href="/contact">
+            Contact
+          </Link>
+        </h2>
       </div>
 
       <footer className={styles.footer}>
